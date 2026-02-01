13e510bef1fe5700ba3c92e7271172bc4194e212
srinu <sopparisrinu637@gmail.com>
2026-02-01 20:06:29 +0530

    HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 
